c49f9495a4bb556269014439738f69c2216f4232
wahyucherryorlove <wahyucherryorlove@gmail.com>
2022-04-20 07:52:27 +0900

    Update Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".navbar-toggle").on("click", function() {
         $(this).toggleClass("navbar-toggle-active");
-        $("#navbar-collapse").toggleClass("h-[220px]");
+        $("#navbar-collapse").toggleClass("h-[260px]");
     });
 
     // Dark Mode
